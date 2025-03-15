a0d40b9047c3247376de52514b31ec0c49942d2f
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-15 19:24:00 +0530

    GpuDriversScreen: missing import

--- app/src/main/java/net/rpcs3/ui/drivers/GpuDriversScreen.kt
+++ app/src/main/java/net/rpcs3/ui/drivers/GpuDriversScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
 import net.rpcs3.utils.GpuDriverHelper
+import net.rpcs3.utils.GpuDriverMetadata
 import java.io.File
 
 @Composable
