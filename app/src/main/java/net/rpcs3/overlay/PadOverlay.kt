63ee2f172bdf603e7e2fdec2cfcc630b57e0dd85
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-03-25 12:08:10 +0530

    Update PadOverlay.kt

--- app/src/main/java/net/rpcs3/overlay/PadOverlay.kt
+++ app/src/main/java/net/rpcs3/overlay/PadOverlay.kt
@@ -2,6 +2,7 @@ package net.rpcs3.overlay
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Rect
 import android.util.AttributeSet
